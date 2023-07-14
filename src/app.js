34702470f1cb32375d0935a662c463be49d263a1
Kartik <138846157+icoKartik@users.noreply.github.com>
2023-07-14 14:51:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,21 +21,21 @@ app.use(express.static(publicStaticDirPath));
 app.get('/',(req,res)=>{
     res.render('index',{
         title : 'WEATHER APP',
-        name : 'PAWAN KUMAR'
+        name : 'Kartik'
     })
 });
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title : 'ABOUT',
-        name : 'PAWAN KUMAR'
+        name : 'kartik'
     })
 });
 
 app.get('/contact',(req,res)=>{
     res.render('contact',{
         title : 'CONTACT US',
-        name : 'PAWAN KUMAR'
+        name : 'Kartik'
     })
 })
 
@@ -43,7 +43,7 @@ app.get('/contact',(req,res)=>{
 app.get('/formsubmitted',(req,res)=>{
     res.render('formsubmitted',{
         title : 'Thanks!',
-        name : 'PAWAN KUMAR'
+        name : 'kartik'
     })
 })
 
@@ -80,7 +80,7 @@ app.get('*',(req,res)=>{
     res.render('404',{
         title : "ERROR 404",
         error : "PAGE NOT FOUND",
-        name : "PAWAN KUMAR"
+        name : "Kartik"
     })
 })
 
